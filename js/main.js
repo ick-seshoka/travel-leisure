93e03e307a476a67b5c691a800e4b1abf091a018
Isaack Seshoka <ick.seshoka@gmail.com>
2018-04-20 11:11:56 +0200

    Removed console logs

--- js/main.js
+++ js/main.js
@@ -10,13 +10,11 @@ function getWeatherInfo() {
     function success(position) {
         var coordsLatitude = position.coords.latitude;
         var coordsLongitude = position.coords.longitude;
-        console.log("lat: " + coordsLatitude + " Lon: " + coordsLongitude);
         var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + coordsLatitude + '&lon=' + coordsLongitude + '&APPID=832a2de82541d49d7f73e85b54090241&units=metric';
 
         fetch(url)
             .then(response => response.json())
             .then(data => {
-            console.log(data);
             locationName.innerHTML = "<p class=\"weather-info\">" + data.name + ", " + data.sys.country + "</p><p class=\"weather-info\">" + data.weather[0].description + "</p>";
             locationTemp.innerHTML = "<p class=\"weather-info\">" + data.main.temp + "&deg;C</p>";
         });
